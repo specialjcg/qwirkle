025f63f74ee6e2630d788bfbba6a07f13cb64cb2
jcgouleau <jean-charles.gouleau@savoye.com>
2021-10-08 18:00:14 +0200

    <typo> change name function 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,12 @@ import {MatButtonModule} from '@angular/material/button';
 import {HttpClientModule} from '@angular/common/http';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { TotalScoreComponent } from './total-score/total-score.component';
-import {MatIconModule} from "@angular/material/icon";
+import {MatIconModule} from '@angular/material/icon';
 import { ResultScoreComponent } from './result-score/result-score.component';
 import { NewGameComponent } from './new-game/new-game.component';
-import {MatOptionModule} from "@angular/material/core";
-import {MatSelectModule} from "@angular/material/select";
-import {MatCardModule} from "@angular/material/card";
+import {MatOptionModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
 import { ChoosePlayerComponent } from './choose-player/choose-player.component';
 import { PlayerTurnComponent } from './player-turn/player-turn.component';
 
