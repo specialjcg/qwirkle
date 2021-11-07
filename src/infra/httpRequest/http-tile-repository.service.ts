1fe555841df1e0c032e6191e9860879723283058
Thomas Vuille <thomas@newtomsoft.fr>
2021-11-07 18:37:42 +0100

    webapi adress

--- src/infra/httpRequest/http-tile-repository.service.ts
+++ src/infra/httpRequest/http-tile-repository.service.ts
@@ -47,71 +47,71 @@ export default class HttpTileRepositoryService {
 
 
   getGames(GameId: number): Promise<BoardGame> {
-    return this.https.post<RestBoard>('https://localhost:5001/Games/get', [GameId], {headers})
+    return this.https.post<RestBoard>('https://qwirkleapi.newtomsoft.fr/Games/get', [GameId], {headers})
       .toPromise().then(response => {console.log(response);return toBoard(response)});
   }
 
   getPlayer(gameId: number, userId: number): Promise<Player> {
-    return this.https.get<string>('https://localhost:5001/Games/Players/' + gameId + '/' + userId, {headers})
+    return this.https.get<string>('https://qwirkleapi.newtomsoft.fr/Games/Players/' + gameId + '/' + userId, {headers})
       .toPromise().then();
 
   }
 
   playTile(tiles: PlayerTile[]): Promise<Rack> {
-    return this.https.post<RestBoard>('https://localhost:5001/Games/PlayTiles/', tiles, {headers})
+    return this.https.post<RestBoard>('https://qwirkleapi.newtomsoft.fr/Games/PlayTiles/', tiles, {headers})
       .toPromise().then();
   }
 
   rackChangeOrder(rack: TileViewModel[]): Promise<Rack> {
-    return this.https.post<RestRack>('https://localhost:5001/Games/ArrangeRack/', rack, {headers})
+    return this.https.post<RestRack>('https://qwirkleapi.newtomsoft.fr/Games/ArrangeRack/', rack, {headers})
       .toPromise().then(response => toChangeRack(response));
   }
 
   playTileSimulation(tiles: PlayerTile[]): Promise<Rack> {
-    return this.https.post<RestBoard>('https://localhost:5001/Games/PlayTilesSimulation/', tiles, {headers})
+    return this.https.post<RestBoard>('https://qwirkleapi.newtomsoft.fr/Games/PlayTilesSimulation/', tiles, {headers})
       .toPromise().then();
   }
 
   swapTile(tiles: PlayerTileToSwap[]): Promise<Rack> {
-    return this.https.post<RestBag>('https://localhost:5001/Games/SwapTiles/', tiles, {headers})
+    return this.https.post<RestBag>('https://qwirkleapi.newtomsoft.fr/Games/SwapTiles/', tiles, {headers})
       .toPromise().then();
   }
 
   skipTurn(playerId: number): Promise<Rack> {
     const player = {id: playerId};
-    return this.https.post<RestSkipTurn>('https://localhost:5001/Games/SkipTurn/', player, {headers})
+    return this.https.post<RestSkipTurn>('https://qwirkleapi.newtomsoft.fr/Games/SkipTurn/', player, {headers})
       .toPromise().then();
   }
 
   getListGames(): Promise<ListGamedId> {
-    return this.https.get<number[]>('https://localhost:5001/Games/ListGameId/', {headers})
+    return this.https.get<number[]>('https://qwirkleapi.newtomsoft.fr/Games/ListGameId/', {headers})
       .toPromise().then(response => toListGamedId(response));
   }
 
   getUsers(): Promise<ListUsersId> {
-    return this.https.get<number[]>('https://localhost:5001/Games/ListUsersId/', {headers})
+    return this.https.get<number[]>('https://qwirkleapi.newtomsoft.fr/Games/ListUsersId/', {headers})
       .toPromise().then(response => toListUsersId(response));
   }
 
   getGamesByUserId(userId: number): Promise<ListGamedId> {
-    return this.https.post<number[]>('https://localhost:5001/Games/ListGamesByUserId/' + userId, {headers})
+    return this.https.post<number[]>('https://qwirkleapi.newtomsoft.fr/Games/ListGamesByUserId/' + userId, {headers})
       .toPromise().then(response => toListGamedId(response));
   }
 
 
 
   newGame(players: number[]): Promise<number[]> {
-    return this.https.post<Player>('https://localhost:5001/Games/', players).toPromise().then();
+    return this.https.post<Player>('https://qwirkleapi.newtomsoft.fr/Games/', players).toPromise().then();
   }
 
   getPlayerNameToPlay(gameId: number): Observable<string> {
-    return this.https.get<string>('https://localhost:5001/Games/GetPlayerNameTurn/' + gameId,
+    return this.https.get<string>('https://qwirkleapi.newtomsoft.fr/Games/GetPlayerNameTurn/' + gameId,
       {responseType: 'text' as 'json'});
 
   }
 
   getWinners(gameId: number): Promise<any> {
-    return this.https.post<any>('https://localhost:5001/Games/Winners/' , [gameId], {headers})
+    return this.https.post<any>('https://qwirkleapi.newtomsoft.fr/Games/Winners/' , [gameId], {headers})
       .toPromise().then();
   }
 }
