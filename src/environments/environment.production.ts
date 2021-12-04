c0af375a489e2de1d3749e282941ea26b8da0511
jcgouleau <jean-charles.gouleau@savoye.com>
2021-12-04 20:08:21 +0100

    <fix> test login in prod

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,13 +1,13 @@
-export const environment = {
-    production: true,
-    backend: {
-        baseURL: 'https://localhost:5001'
-    }
-};
-
 // export const environment = {
 //     production: true,
 //     backend: {
-//         baseURL: 'https://qwirkleapi.newtomsoft.fr'
+//         baseURL: 'https://localhost:5001'
 //     }
 // };
+
+export const environment = {
+    production: true,
+    backend: {
+        baseURL: 'https://qwirkleapi.newtomsoft.fr'
+    }
+};
