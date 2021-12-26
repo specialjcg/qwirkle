69a41466a4d30c2b9226bab7d0141babfd4f6023
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-26 11:23:10 +0100

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    backend: {
+        // baseURL: 'https://qwirkleapi.newtomsoft.fr'
+        baseURL: 'https://localhost:5001'
+    }
 };
 
 /*
