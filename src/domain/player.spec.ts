9e7ff31da25a9b212d0f52467d0e40a28da5dbb8
jcgouleau <jean-charles.gouleau@savoye.com>
2022-01-07 09:02:01 +0100

    <test> add bag to test 2

--- src/domain/player.spec.ts
+++ src/domain/player.spec.ts
@@ -1,13 +1,14 @@
 import {
-  fromSwap,
-  fromBoard,
-  Player,
-  RestBoard,
-  RestRack,
-  toBoard,
-  toChangeRack,
-  toPlayers,
-  toWebPlayer, TilesOnBag
+    fromSwap,
+    fromBoard,
+    Player,
+    RestBoard,
+    RestRack,
+    toBoard,
+    toChangeRack,
+    toPlayers,
+    toWebPlayer,
+    TilesOnBag
 } from './player';
 import { Tile } from './Tile';
 
@@ -650,6 +651,9 @@ describe('player', () => {
             ]
         };
         expect(toBoard(restboard)).toEqual({
+            bag: {
+                tiles: []
+            },
             boards: [
                 {
                     color: 1,
@@ -1054,48 +1058,48 @@ describe('player', () => {
         const playerId = 1;
 
         expect(fromSwap(result, playerId)).toEqual([
-          {
-            "X": 3,
-            "Y": -1,
-            "color": 1,
-            "gameId": 1,
-            "shape": 2
-          },
-          {
-            "X": 4,
-            "Y": -1,
-            "color": 1,
-            "gameId": 1,
-            "shape": 5
-          },
-          {
-            "X": 7,
-            "Y": 3,
-            "color": 3,
-            "gameId": 1,
-            "shape": 4
-          },
-          {
-            "X": 2,
-            "Y": 0,
-            "color": 4,
-            "gameId": 1,
-            "shape": 1
-          },
-          {
-            "X": 1,
-            "Y": 0,
-            "color": 4,
-            "gameId": 1,
-            "shape": 3
-          },
-          {
-            "X": 3,
-            "Y": 2,
-            "color": 5,
-            "gameId": 1,
-            "shape": 6
-          }
+            {
+                X: 3,
+                Y: -1,
+                color: 1,
+                gameId: 1,
+                shape: 2
+            },
+            {
+                X: 4,
+                Y: -1,
+                color: 1,
+                gameId: 1,
+                shape: 5
+            },
+            {
+                X: 7,
+                Y: 3,
+                color: 3,
+                gameId: 1,
+                shape: 4
+            },
+            {
+                X: 2,
+                Y: 0,
+                color: 4,
+                gameId: 1,
+                shape: 1
+            },
+            {
+                X: 1,
+                Y: 0,
+                color: 4,
+                gameId: 1,
+                shape: 3
+            },
+            {
+                X: 3,
+                Y: 2,
+                color: 5,
+                gameId: 1,
+                shape: 6
+            }
         ]);
     });
 });
