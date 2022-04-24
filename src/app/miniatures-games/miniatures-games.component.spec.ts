259ea41232d188adb9440a1cc3189d28546adebe
jcgouleau <jean-charles.gouleau@savoye.com>
2022-04-24 18:26:10 +0200

    <test > add test miniature

--- src/app/miniatures-games/miniatures-games.component.spec.ts
+++ src/app/miniatures-games/miniatures-games.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MiniaturesGamesComponent } from './miniatures-games.component';
+import {MenuComponent} from "../menu/menu.component";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {MatMenuModule} from "@angular/material/menu";
+import HttpTileRepositoryService from "../../infra/httpRequest/http-tile-repository.service";
+import {AuthGuard} from "../auth/auth.guard";
 
 describe('MiniaturesGamesComponent', () => {
   let component: MiniaturesGamesComponent;
@@ -8,7 +14,9 @@ describe('MiniaturesGamesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MiniaturesGamesComponent ]
+      declarations: [ MiniaturesGamesComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule, MatMenuModule],
+      providers: [HttpTileRepositoryService, AuthGuard]
     })
     .compileComponents();
   });
